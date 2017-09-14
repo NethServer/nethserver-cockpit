681105f5b5e4ec25eb1b1200383f7c804d963fc1
Giovanni Bezicheri <giovanni.bezicheri@nethesis.it>
2017-09-14 11:42:19 +0200

    Add notification support.

--- ui/ns-system-ui/app/scripts/controllers/init.js
+++ ui/ns-system-ui/app/scripts/controllers/init.js
@@ -9,9 +9,30 @@
  */
 angular.module('systemAngularApp')
   .controller('InitCtrl', function ($scope) {
+    $('body').show();
+
     $scope.stats = {
       tasks: [{}],
       updates: [],
       errors: []
     };
+
+    $scope.iconMap = {
+      'info' : 'info',
+      'error' : 'error-circle-o',
+      'success' : 'ok',
+      'warning' : 'warning-triangle-o'
+    };
+
+    $scope.notifications = [];
+
+    $scope.addNotification = function (notification) {
+      $scope.notifications.push(notification);
+    }
+
+    $scope.removeNotification = function (index) {
+      $scope.notifications.splice(index, 1);
+    }
+
+    // $scope.addNotification({ message: 'test1', status: 'info', action: 'check',  url: 'http://www.patternfly.org' });
   });
