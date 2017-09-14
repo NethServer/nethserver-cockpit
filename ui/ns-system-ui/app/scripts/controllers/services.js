1eeadfb51e8351c61609367c3087b063e8c62f8d
Giovanni Bezicheri <giovanni.bezicheri@nethesis.it>
2017-09-14 17:49:32 +0200

    Add services stub.

--- ui/ns-system-ui/app/scripts/controllers/services.js
+++ ui/ns-system-ui/app/scripts/controllers/services.js
@@ -8,10 +8,54 @@
  * Controller of the systemAngularApp
  */
 angular.module('systemAngularApp')
-  .controller('ServicesCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('ServicesCtrl', function ($scope) {
+    nethserver.System.services.getAllServices(function (services) {
+      $scope.allServices = services;
+
+      // applying scope
+      // $scope.$apply();
+    }, function (err) {
+      console.error("couldn't read services: " + err);
+    });
+
+    $scope.enable = function () {
+      nethserver.System.services.enableService(function () {
+
+      }, function (err) {
+        console.error(err);
+      });
+    }
+
+    $scope.disable = function () {
+      nethserver.System.services.disableService(function () {
+
+      }, function (err) {
+        console.error(err);
+      });
+    }
+
+    $scope.start = function () {
+      nethserver.System.services.startService(function () {
+
+      }, function (err) {
+        console.error(err);
+      });
+    }
+
+    $scope.stop = function () {
+      nethserver.System.services.stopService(function () {
+
+      }, function (err) {
+        console.error(err);
+      });
+    }
+
+    $scope.restart = function () {
+      console.log('restart');
+      nethserver.System.services.restartService(function () {
+
+      }, function (err) {
+        console.error(err);
+      });
+    }
   });
