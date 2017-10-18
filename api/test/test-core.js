22fdee800c59aa430b2438062acf2cfd2d0f79da
Davide Principi <davide.principi@nethesis.it>
2017-10-18 12:37:12 +0200

    Fix test "getObject on a non-existing key"

--- api/test/test-core.js
+++ api/test/test-core.js
@@ -317,7 +317,7 @@ describe('Also, the object returned by getDatabase()', function() {
                 return tdb.save();
             }).
             then(function(){
-                (function(){tdb.getObject('keytest2');}).should.throw('NotFound');
+                (function(){tdb.getObject('keytest2')}).should.throw(nethserver.Error, {type: 'NotFound'});
             });
     });
 
