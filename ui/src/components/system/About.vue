a48f53f60ce9ddfeffdf56f946ef861e9596a8b5
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2018-11-14 15:26:05 +0100

    ui. added about page

--- ui/src/components/system/About.vue
+++ ui/src/components/system/About.vue
@@ -1,7 +1,37 @@
 <template>
   <div>
     <h2>{{$t('about.title')}}</h2>
-    <div class="page-container">
+    <div class="blank-slate-pf white-background" id="">
+      <h1 class="logo">
+        <img class="img" src="static/assets/logo.svg">
+      </h1>
+      <h2>
+        {{$t('about.description_1')}}
+      </h2>
+      <p>
+        {{$t('about.description_2')}}
+      </p>
+      <p>
+        {{$t('about.visit_the')}} <a target="_blank" href="https://www.nethserver.org/">{{$t('about.official_site')}}</a>.
+      </p>
+      <p class="div-section"></p>
+      <p>
+        {{$t('about.description_3')}}
+      </p>
+      <div class="blank-slate-pf-main-action">
+        <a target="_blank" href="https://community.nethserver.org/" class="btn btn-primary btn-lg">{{$t('about.community')}}</a>
+      </div>
+      <div class="blank-slate-pf-secondary-action">
+        <a target="_blank" href="http://docs.nethserver.org" class="btn btn-default ">{{$t('about.documentation')}}</a>
+        <a target="_blank" href="https://wiki.nethserver.org" class="btn btn-default ">{{$t('about.wiki')}}</a>
+      </div>
+      <p class="div-section"></p>
+      <p>
+        {{$t('about.description_4')}}
+      </p>
+      <div class="blank-slate-pf-main-action">
+        <a target="_blank" href="https://my.nethserver.com/login" class="btn btn-primary btn-lg">{{$t('about.subscription')}}</a>
+      </div>
     </div>
   </div>
 </template>
@@ -16,4 +46,18 @@ export default {
 </script>
 
 <style>
+.logo {
+  margin-top: -25%;
+  margin-bottom: -15%;
+}
+
+.img {
+  width: 90%;
+}
+
+.div-section {
+  border-top: 1px solid #d1d1d1;
+  margin-top: 3%;
+  margin-bottom: 3%;
+}
 </style>
