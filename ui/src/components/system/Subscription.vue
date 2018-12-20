c6aa721f8ec570ae83a54da0ee25dd8823fa9691
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2018-12-20 15:43:04 +0100

    ui. show if there is connection error

--- ui/src/components/system/Subscription.vue
+++ ui/src/components/system/Subscription.vue
@@ -6,23 +6,43 @@
       <h3>{{$t('subscription.registration')}}</h3>
       <div v-if="!SubscriptionConfig.status" class="alert alert-info alert-dismissable">
         <span class="pficon pficon-info"></span>
-        {{$t('subscription.registration_description')}}. <br /><br />
-        <a target="_blank" :href="SubscriptionConfig.pricingUrl" class="btn btn-primary btn-lg">{{$t('subscription.subscribe')}}</a>
-        <br /><br />
-        {{$t('subscription.registration_description_2')}}. <br />
+        {{$t('subscription.registration_description')}}.
+        <br>
+        <br>
+        <a
+          target="_blank"
+          :href="SubscriptionConfig.pricingUrl"
+          class="btn btn-primary btn-lg"
+        >{{$t('subscription.subscribe')}}</a>
+        <br>
+        <br>
+        {{$t('subscription.registration_description_2')}}.
+        <br>
       </div>
-      <form v-if="!SubscriptionConfig.status" class="form-horizontal" v-on:submit.prevent="saveSubscription(SubscriptionConfig)">
+      <form
+        v-if="!SubscriptionConfig.status"
+        class="form-horizontal"
+        v-on:submit.prevent="saveSubscription(SubscriptionConfig)"
+      >
         <div :class="['form-group', SubscriptionConfig.errors.secret.hasError ? 'has-error' : '']">
-          <label class="col-sm-2 control-label" for="textInput-modal-markup">{{$t('subscription.token')}}</label>
+          <label
+            class="col-sm-2 control-label"
+            for="textInput-modal-markup"
+          >{{$t('subscription.token')}}</label>
           <div class="col-sm-7">
             <input required type="text" v-model="SubscriptionConfig.secret" class="form-control">
-            <span v-if="SubscriptionConfig.errors.secret.hasError" class="help-block">{{$t('validation.validation_failed')}}:
-              {{$t('validation.'+SubscriptionConfig.errors.secret.message)}}</span>
+            <span v-if="SubscriptionConfig.errors.secret.hasError" class="help-block">
+              {{$t('validation.validation_failed')}}:
+              {{$t('validation.'+SubscriptionConfig.errors.secret.message)}}
+            </span>
           </div>
         </div>
         <div class="form-group">
           <label class="col-sm-2 control-label" for="textInput-modal-markup">
-            <div v-if="SubscriptionConfig.isLoading" class="spinner spinner-sm form-spinner-loader adjust-top-loader"></div>
+            <div
+              v-if="SubscriptionConfig.isLoading"
+              class="spinner spinner-sm form-spinner-loader adjust-top-loader"
+            ></div>
           </label>
           <div class="col-sm-5">
             <button class="btn btn-primary" type="submit">{{$t('register')}}</button>
@@ -31,45 +51,80 @@
       </form>
       <form v-if="SubscriptionConfig.status" class="form-horizontal">
         <div class="form-group compact">
-          <label class="col-sm-2 control-label" for="textInput-modal-markup">{{$t('subscription.system_id')}}</label>
+          <label
+            class="col-sm-2 control-label"
+            for="textInput-modal-markup"
+          >{{$t('subscription.system_id')}}</label>
           <div class="col-sm-7 adjust-li">
             <pre class="pre-inline">{{SubscriptionConfig.status.uuid}}</pre>
           </div>
         </div>
         <div class="form-group compact">
-          <label class="col-sm-2 control-label" for="textInput-modal-markup">{{$t('subscription.plan')}}</label>
+          <label
+            class="col-sm-2 control-label"
+            for="textInput-modal-markup"
+          >{{$t('subscription.plan')}}</label>
           <div class="col-sm-7 adjust-li">
-            <p>{{SubscriptionConfig.status.subscription &&
-              SubscriptionConfig.status.subscription.subscription_plan.name}}</p>
+            <p>
+              {{SubscriptionConfig.status.subscription &&
+              SubscriptionConfig.status.subscription.subscription_plan.name}}
+            </p>
           </div>
         </div>
         <div class="form-group compact">
-          <label class="col-sm-2 control-label" for="textInput-modal-markup">{{$t('subscription.expiration')}}</label>
+          <label
+            class="col-sm-2 control-label"
+            for="textInput-modal-markup"
+          >{{$t('subscription.expiration')}}</label>
           <div class="col-sm-7 adjust-li">
-            <p>{{SubscriptionConfig.status.subscription && SubscriptionConfig.status.subscription.valid_until |
-              dateFormat}}</p>
+            <p>
+              {{SubscriptionConfig.status.subscription && SubscriptionConfig.status.subscription.valid_until |
+              dateFormat}}
+            </p>
           </div>
         </div>
         <div class="form-group compact">
-          <label class="col-sm-2 control-label" for="textInput-modal-markup">{{$t('subscription.status')}}</label>
+          <label
+            class="col-sm-2 control-label"
+            for="textInput-modal-markup"
+          >{{$t('subscription.status')}}</label>
           <div class="col-sm-7 adjust-li">
-            <span :class="SubscriptionConfig.status.subscription && SubscriptionConfig.status.subscription.status == 'valid' ? 'pficon pficon-ok' : 'pficon pficon-error-circle-o'"></span>
+            <span
+              :class="SubscriptionConfig.status.subscription && SubscriptionConfig.status.subscription.status == 'valid' ? 'pficon pficon-ok' : 'pficon pficon-error-circle-o'"
+            ></span>
           </div>
         </div>
         <div class="form-group compact">
-          <label class="col-sm-2 control-label" for="textInput-modal-markup">{{$t('subscription.cloud_portal')}}</label>
+          <label
+            class="col-sm-2 control-label"
+            for="textInput-modal-markup"
+          >{{$t('subscription.cloud_portal')}}</label>
           <div class="col-sm-7 adjust-li">
-            <a v-if="SubscriptionConfig.status" :href="SubscriptionConfig.portalURL" target="_blank" class="alert-link">{{$t('subscription.access')}}</a>
+            <a
+              v-if="SubscriptionConfig.status"
+              :href="SubscriptionConfig.portalURL"
+              target="_blank"
+              class="alert-link"
+            >{{$t('subscription.access')}}</a>
           </div>
         </div>
       </form>
       <h3 v-if="SubscriptionConfig.status">{{$t('subscription.connection')}}</h3>
       <form v-if="SubscriptionConfig.status" class="form-horizontal">
         <div class="form-group compact">
-          <label class="col-sm-2 control-label" for="textInput-modal-markup">{{$t('subscription.check_connection')}}</label>
+          <label
+            class="col-sm-2 control-label"
+            for="textInput-modal-markup"
+          >{{$t('subscription.check_connection')}}</label>
           <div class="col-sm-2 adjust-li">
-            <button :disabled="view.isChecking" @click="checkConnection()" type="button" class="btn btn-primary">{{$t('subscription.check')}}</button>
+            <button
+              :disabled="view.isChecking"
+              @click="checkConnection()"
+              type="button"
+              class="btn btn-primary"
+            >{{$t('subscription.check')}}</button>
             <span v-if="view.isChecked && !view.isChecking" class="fa fa-check green copy-ok"></span>
+            <span v-if="view.isCheckFail && !view.isChecking" class="fa fa-times red copy-ok"></span>
             <div v-if="view.isChecking" class="spinner spinner-sm adjust-spinner-top"></div>
           </div>
         </div>
@@ -126,7 +181,8 @@ export default {
         isLoaded: false,
         isAuth: false,
         isChecking: false,
-        isChecked: false
+        isChecked: false,
+        isCheckFail: false
       },
       SubscriptionConfig: {
         isLoading: false,
@@ -236,7 +292,9 @@ export default {
       context.view.isChecking = true;
       context.exec(
         ["system-subscription/update"],
-        { action: "send" },
+        {
+          action: "send"
+        },
         null,
         function(success) {
           try {
@@ -246,12 +304,16 @@ export default {
           }
           context.view.isChecking = false;
           context.view.isChecked = true;
+          context.view.isCheckFail = false;
           setTimeout(function() {
             context.view.isChecked = false;
           }, 2000);
         },
         function(error) {
           console.error(error);
+          context.view.isChecking = false;
+          context.view.isChecked = false;
+          context.view.isCheckFail = true;
         }
       );
     }
