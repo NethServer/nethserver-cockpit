7a38ad3591d8780648b2e3c3e18aaf97872d53c6
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2019-08-28 12:04:49 +0200

    ui. added about page with enterprise info

--- ui/src/components/system/About.vue
+++ ui/src/components/system/About.vue
@@ -1,36 +1,55 @@
 <template>
   <div>
     <h2>{{$t('about.title')}}</h2>
-    <div class="blank-slate-pf white-background" id="">
+
+    <div v-if="!view.isLoaded" class="spinner spinner-lg view-spinner"></div>
+
+    <div v-if="view.isLoaded" class="blank-slate-pf white-background" id>
       <h1 class="logo">
-        <img class="img" src="assets/logo.svg">
+        <img
+          :class="['img', enterprise ? 'adjust-img' : '']"
+          :src="enterprise ? 'assets/logo_ent.png' : 'assets/logo.svg'"
+        />
       </h1>
-      <h2>
-        {{$t('about.description_1')}}
-      </h2>
+      <h2>{{$t('about.description_1')}}</h2>
+      <p>{{$t('about.description_2')}}</p>
       <p>
-        {{$t('about.description_2')}}
-      </p>
-      <p>
-        {{$t('about.visit_the')}} <a target="_blank" href="https://www.nethserver.org/">{{$t('about.official_site')}}</a>.
+        {{$t('about.visit_the')}}
+        <a
+          target="_blank"
+          :href="enterprise ? 'https://nethesis.it' : 'https://www.nethserver.org'"
+        >{{$t('about.official_site')}}</a>.
       </p>
       <p class="div-section"></p>
-      <p>
-        {{$t('about.description_3')}}
-      </p>
+      <p>{{enterprise ? $t('about.description_3_2') : $t('about.description_3_1')}}</p>
       <div class="blank-slate-pf-main-action">
-        <a target="_blank" href="https://community.nethserver.org/" class="btn btn-primary btn-lg">{{$t('about.community')}}</a>
+        <a
+          target="_blank"
+          :href="enterprise ? 'http://helpdesk.nethesis.it' : 'https://community.nethserver.org/'"
+          class="btn btn-primary btn-lg"
+        >{{enterprise ? $t('about.helpdesk') : $t('about.community')}}</a>
       </div>
       <div class="blank-slate-pf-secondary-action">
-        <a target="_blank" href="http://docs.nethserver.org" class="btn btn-default ">{{$t('about.documentation')}}</a>
-        <a target="_blank" href="https://wiki.nethserver.org" class="btn btn-default ">{{$t('about.wiki')}}</a>
+        <a
+          target="_blank"
+          :href="enterprise ? 'https://docs.nethesis.it' : 'http://docs.nethserver.org'"
+          class="btn btn-default"
+        >{{$t('about.documentation')}}</a>
+        <a
+          v-if="!enterprise"
+          target="_blank"
+          href="https://wiki.nethserver.org"
+          class="btn btn-default"
+        >{{$t('about.wiki')}}</a>
       </div>
-      <p class="div-section"></p>
-      <p>
-        {{$t('about.description_4')}}
-      </p>
-      <div class="blank-slate-pf-main-action">
-        <a target="_blank" href="https://my.nethserver.com/login" class="btn btn-primary btn-lg">{{$t('about.subscription')}}</a>
+      <p v-if="!enterprise" class="div-section"></p>
+      <p v-if="!enterprise">{{$t('about.description_4')}}</p>
+      <div v-if="!enterprise" class="blank-slate-pf-main-action">
+        <a
+          target="_blank"
+          href="https://my.nethserver.com/login"
+          class="btn btn-primary btn-lg"
+        >{{$t('about.subscription')}}</a>
       </div>
     </div>
   </div>
@@ -42,9 +61,39 @@ export default {
   mounted() {
     $("#app").css("background", "");
     $("#app").css("color", "");
+    this.getInfo();
   },
   data() {
-    return {};
+    return {
+      view: {
+        isLoaded: false
+      },
+      enterprise: false
+    };
+  },
+  methods: {
+    getInfo() {
+      var context = this;
+      nethserver.exec(
+        ["system-subscription/read"],
+        null,
+        null,
+        function(success) {
+          try {
+            success = JSON.parse(success);
+            context.view.isLoaded = true;
+            context.enterprise =
+              success.configuration.PortalURL.indexOf("nethesis") > -1;
+          } catch (e) {
+            console.error(e);
+            context.view.isLoaded = true;
+          }
+        },
+        function(error) {
+          console.error(error);
+        }
+      );
+    }
   }
 };
 </script>
@@ -57,6 +106,12 @@ export default {
 
 .img {
   width: 90%;
+}
+
+.adjust-img {
+  margin-top: 22%;
+  margin-bottom: 19%;
+  width: 40% !important;
 }
 
 .div-section {
