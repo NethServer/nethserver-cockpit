1b8053dbf464fdd0545b7d0772d256c3118f021e
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2020-01-20 12:37:43 +0100

    ui. fixed BondOptMode in interface creation (#210)
    Nethserver/dev#6032

--- ui/src/components/system/Network.vue
+++ ui/src/components/system/Network.vue
@@ -2571,7 +2571,7 @@ export default {
             console.error(e);
           }
           context.wizard.type.bond.modes = success;
-          context.wizard.type.bond.mode = success[0];
+          context.wizard.type.bond.mode = context.wizard.type.bond.mode.length > 0 ? context.wizard.type.bond.mode : success[0];
         },
         function(error) {
           console.error(error);
@@ -3201,7 +3201,7 @@ export default {
         devices: context.wizard.type[context.wizard.type.choice].devices,
         BondOptMode:
           context.wizard.type.choice == "bond"
-            ? context.wizard.type.bond.mode.value
+            ? context.wizard.type.bond.mode
             : null,
         tag:
           context.wizard.type.choice == "vlan"
